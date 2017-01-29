cfd1376eb85f217aaece3c03e28c344f5732ad2c
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-01-30 00:13:38 +0800

    additional ui

--- test/test-list.jsx
+++ test/test-list.jsx
@@ -1,10 +1,49 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export default class TestList extends Component {
-    render () {
+    render() {
         return (
             <div>
                 <h3>List</h3>
+                <List
+                    header={{
+                        "icon": "fa fa-home",
+                        "label": "List of Fruits",
+                        "description": "This is a description"
+                    }}
+                    list={[
+                        {
+                            "value": "Orange"
+                        },
+                        {
+                            "value": "Peach"
+                        },
+                        {
+                            "value": "Apple"
+                        },
+                        {
+                            "value": "Banana"
+                        },
+                        {
+                            "value": "Mango"
+                        },
+                        {
+                            "value": "Blueberry"
+                        },
+                        {
+                            "value": "Avocado"
+                        },
+                        {
+                            "value": "Apple"
+                        },
+                        {
+                            "value": "Blackberry"
+                        },
+                        {
+                            "value": "Lychee"
+                        }
+                    ]}
+                    />
             </div>
         )
     }
--- test/test-select.jsx
+++ test/test-select.jsx
@@ -1,10 +1,37 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export default class TestSelect extends Component {
-    render () {
+    render() {
         return (
             <div>
                 <h3>Select</h3>
+                <Select
+                    header={{
+                        "icon": "fa fa-home",
+                        "label": "List of Fruits"
+                        
+                    }}
+
+                    list={[
+                        {
+                            "value": "Orange"
+                        },
+                        {
+                            "value": "Peach"
+                        },
+                        {
+                            "value": "Apple"
+                        },
+                        {
+                            "value": "Banana"
+                        },
+                        {
+                            "value": "Mango"
+                        }
+                    ]}
+                    >
+
+                </Select>
             </div>
         )
     }
